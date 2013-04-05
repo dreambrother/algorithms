813d6d2418053ca3ccf946dc2d31c0d084e7d7fe
dreambrother <nikshmakov@gmail.com>
2013-04-05 10:39:17 +0400

    add kNN algorithm

---
+++ machine_learning/knn.py
@@ -0,0 +1,21 @@
+__author__ = 'nik'
+
+import math
+
+K = 5
+
+def classify(item, training_set):
+    distances = []
+    for training_item in training_set:
+        distance = math.sqrt((math.pow(training_item[1] - item[0], 2) + math.pow(training_item[2] - item[1], 2)))
+        distances.append((training_item[0], distance))
+
+    distances.sort(cmp = lambda x, y: 1 if x[1] > y[1] else -1)
+    class_count = {}
+    for i in range(0, K):
+        genre = distances[i][0]
+        if not class_count.has_key(genre):
+            class_count[genre] = 0
+        class_count[genre] += 1
+    result = max(class_count, key=class_count.get)
+    return result
---
+++ machine_learning/knn_test.py
@@ -0,0 +1,21 @@
+__author__ = 'nik'
+
+import unittest
+import knn
+
+class TestKnn(unittest.TestCase):
+
+    # (genre, # of kisses, # of kicks)
+    training_set = [('action', 3, 145), ('action', 5, 160), ('action', 1, 151),
+                    ('romance', 160, 0), ('romance', 140, 2), ('romance', 170, 1)]
+
+    def test_action_recognition(self):
+        actual = knn.classify((0, 155), self.training_set)
+        self.assertEqual('action', actual)
+
+    def test_romance_recognition(self):
+        actual = knn.classify((180, 8), self.training_set)
+        self.assertEqual('romance', actual)
+
+if __name__ == '__main__':
+    unittest.main()
