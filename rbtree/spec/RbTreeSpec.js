10d6a384194378650e2ad53f46b9dbd49b3c6ecb
NShmakov <nshmakov@yotateam.com>
2013-08-30 16:42:53 +0400

    add not completed Red-Black tree implementation

--- rbtree/spec/RbTreeSpec.js
+++ rbtree/spec/RbTreeSpec.js
@@ -38,4 +38,50 @@ describe("Red-black tree", function() {
         var tree = new RbTree(rootNode);
         expect(tree.get(5)).toBeUndefined();
     });
+
+    it("should add root node into tree", function() {
+        var tree = new RbTree();
+        tree.add(1, "val");
+
+        expect(tree.get(1)).toBe("val");
+    });
+
+    it("should add node into right subtree", function() {
+        var tree = new RbTree();
+        tree.add(1, "val1");
+        tree.add(2, "val2");
+
+        expect(tree.get(2)).toBe("val2");
+    });
+
+    it("should restore invariants after right violation", function() {
+        var tree = new RbTree();
+        tree.add(1, "root");
+        tree.add(2, "first child");
+        tree.add(3, "second child");
+        tree.add(4, "third child");
+
+        var rootNode = tree._rootNode;
+        expect(rootNode.color).toBe(Color.BLACK);
+
+        var secondChild = rootNode.rightChild;
+        expect(secondChild.value).toBe("second child");
+        expect(secondChild.color).toBe(Color.BLACK)
+
+        var firstChild = secondChild.leftChild;
+        expect(firstChild.value).toBe("first child");
+        expect(firstChild.color).toBe(Color.RED);
+
+        var thirdChild = secondChild.rightChild;
+        expect(thirdChild.value).toBe("third child");
+        expect(thirdChild.color).toBe(Color.RED);
+    });
+
+    it("should add node into left subtree", function() {
+        var tree = new RbTree();
+        tree.add(2, "val1");
+        tree.add(1, "val2");
+
+        expect(tree.get(1)).toBe("val2");
+    });
 });
--- rbtree/src/rbtree.js
+++ rbtree/src/rbtree.js
@@ -1,8 +1,14 @@
 var RbTree = (function() {
 
-    var RbTree = function(rootNode) {
+    var Constructor = function(rootNode) {
         this._rootNode = rootNode;
-    }
+    };
+
+    Constructor.prototype.get = function(key) {
+        if (this._rootNode !== undefined) {
+            return recursiveFind(this._rootNode, key);
+        }
+    };
 
     var recursiveFind = function(fromNode, key) {
         if (fromNode.key === key) {
@@ -12,27 +18,108 @@ var RbTree = (function() {
         } else if (key > fromNode.key && fromNode.rightChild) {
             return recursiveFind(fromNode.rightChild, key);
         }
-    }
+    };
 
-    RbTree.prototype.get = function(key) {
-        if (this._rootNode) {
-            return recursiveFind(this._rootNode, key);
+    Constructor.prototype.add = function(key, value) {
+        if (this._rootNode === undefined) {
+            this._rootNode = new Node(undefined, key, value, Color.BLACK);
+        } else {
+            recursiveAdd(this._rootNode, new Node(this._rootNode, key, value, Color.RED));
         }
-    }
+    };
 
-    return RbTree;
+    var recursiveAdd = function(toNode, node) {
+        if (node.key !== toNode.key) {
+            if (node.key > toNode.key) {
+                if (toNode.rightChild === undefined) {
+                    toNode.rightChild = node;
+                    restoreInvariants(node);
+                } else {
+                    recursiveAdd(toNode.rightChild, node);
+                }
+            } else {
+                if (toNode.leftChild === undefined) {
+                    toNode.leftChild = node;
+                    restoreInvariants(node);
+                } else {
+                    recursiveAdd(toNode.leftChild, node);
+                }
+            }
+        }
+    };
+
+    var restoreInvariants = function(node) {
+        while (isNotRootNodeAndHasRedParent(node)) {
+            var uncle = getUncle(node);
+            if(uncle !== undefined && uncle.color === Color.RED) {
+                node.color = Color.BLACK;
+                node.parent.color = Color.BLACK;
+            } else {
+
+            }
+            node = node.parent.parent;
+        }
+    };
+
+    var isNotRootNodeAndHasRedParent = function(node) {
+        return node.parent !== undefined && node.parent.color === Color.RED;
+    };
+
+    var getUncle = function(node) {
+        var parent = node.parent;
+        var grandfather = parent.parent;
+        if (parent === grandfather.rightChild) {
+            return grandfather.leftChild;
+        } else {
+            return grandfather.rightChild;
+        }
+    };
+
+    var leftRotation = function(node) {
+        var rightChild = node.rightChild;
+        if (rightChild === undefined) {
+            throw new Error("Left rotation is denied");
+        }
+        var parent = node.parent;
+        if (parent.rightChild === node) {
+            parent.rightChild = rightChild;
+        } else {
+            parent.leftChild = rightChild;
+        }
+        var tmp = rightChild.leftChild;
+        rightChild.leftChild = node;
+        node.rightChild = tmp;
+    };
+
+    var rightRotation = function(node) {
+        var leftChild = node.leftChild;
+        if (leftChild === undefined) {
+            throw new Error("Right rotation is denied");
+        }
+        var parent = node.parent;
+        if (parent.rightChild === node) {
+            parent.rightChild = leftChild;
+        } else {
+            parent.leftChild = leftChild;
+        }
+        var tmp = leftChild.rightChild;
+        leftChild.rightChild = node;
+        node.rightChild = tmp;
+    };
+
+    return Constructor;
 })();
 
 var Node = (function() {
 
-    var Node = function(parent, key, value, color) {
+    var Constructor = function(parent, key, value, color) {
         this.parent = parent;
         this.key = key;
         this.value = value;
         this.color = color;
     };
 
-    return Node;
+    return Constructor;
 })();
 
 var Color = {
